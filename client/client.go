2d1db5bb0f5b389d72e8c16c7495d138fb65d1d3
abdulaziz-go <abdulaziomonov4@gmail.com>
2025-09-20 15:00:31 +0500

    client struct done

--- client/client.go
+++ client/client.go
@@ -1,1 +1,61 @@
 package client
+
+import (
+	"bufio"
+	"fmt"
+	"gamehomework/tlv"
+	"net"
+	"os"
+)
+
+type Client struct {
+	conn net.Conn
+	name string
+}
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+func Start() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Printf("error while connecting %v", err)
+		return
+	}
+	defer conn.Close()
+
+	client := &Client{conn: conn}
+	client.run()
+}
+
+func (c *Client) run() {
+	fmt.Printf("Enter your name: ")
+	reader := bufio.NewReader(os.Stdin)
+	name, _ := reader.ReadString('\n')
+	c.name = name
+
+	c.conn.Write(tlv.Encode(tlv.JOIN, []byte(c.name)))
+
+	go c.listenMessage()
+}
+
+func (c *Client) listenMessage() {
+	for {
+		buf := make([]byte, 1024)
+		n, err := c.conn.Read(buf)
+
+		if err != nil {
+			return
+		}
+		msgType, data := tlv.Decode(buf[:n])
+
+		switch msgType {
+		case tlv.STATE:
+			c.displayBoard(data)
+		case tlv.WIN:
+			fmt.Println(string(data))
+			return
+		}
+	}
+}
