4c345085e7c58e52e67e99649b17add1265feacc
abdulaziz-go <abdulaziomonov4@gmail.com>
2025-09-20 15:10:47 +0500

    client done

--- client/client.go
+++ client/client.go
@@ -6,6 +6,8 @@ import (
 	"gamehomework/tlv"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Client struct {
@@ -38,6 +40,8 @@ func (c *Client) run() {
 	c.conn.Write(tlv.Encode(tlv.JOIN, []byte(c.name)))
 
 	go c.listenMessage()
+
+	c.handleInput(reader)
 }
 
 func (c *Client) listenMessage() {
@@ -52,10 +56,58 @@ func (c *Client) listenMessage() {
 
 		switch msgType {
 		case tlv.STATE:
-			c.displayBoard(data)
+			if len(data) >= 11 {
+				c.displayBoard(data[:9], data[9], data[10])
+			}
 		case tlv.WIN:
 			fmt.Println(string(data))
 			return
 		}
+	}
+}
+
+func (c *Client) displayBoard(board []byte, turn, winner byte) {
+	symbols := map[byte]string{tlv.EMPTY: " ", tlv.X: "X", tlv.O: "O"}
+	fmt.Println(" " + symbols[board[0]] + " | " + symbols[board[1]] + " | " + symbols[board[2]])
+	fmt.Println("-----------")
+	fmt.Println(" " + symbols[board[3]] + " | " + symbols[board[4]] + " | " + symbols[board[5]])
+	fmt.Println("-----------")
+	fmt.Println(" " + symbols[board[6]] + " | " + symbols[board[7]] + " | " + symbols[board[8]])
+
+	fmt.Println("Enter moves as numbers 1-9:")
+	fmt.Println("1 2 3")
+	fmt.Println("4 5 6")
+	fmt.Println("7 8 9")
+	if winner == 3 {
+		fmt.Println("\n DRAW!")
+	} else if winner != tlv.EMPTY {
+		fmt.Printf("\n %s wins", symbols[winner])
+	} else {
+		fmt.Printf("\nCurrent turn: %s\n", symbols[turn])
+	}
+}
+
+func (c *Client) handleInput(reader *bufio.Reader) {
+	fmt.Println("Connected! Enter moves as numbers 1-9:")
+	fmt.Println("1 2 3")
+	fmt.Println("4 5 6")
+	fmt.Println("7 8 9")
+
+	for {
+		fmt.Print("Your move: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "q" {
+			break
+		}
+
+		pos, err := strconv.Atoi(input)
+		if err != nil || pos < 1 || pos > 9 {
+			fmt.Println("Enter a number 1-9")
+			continue
+		}
+
+		c.conn.Write(tlv.Encode(tlv.MOVE, []byte{byte(pos - 1)}))
 	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"gamehomework/client"
 	"gamehomework/server"
 	"os"
 )
