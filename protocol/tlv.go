cfc3d294b788b91e06a9c9d24cc878b3b9abacf9
abdulaziz-go <abdulaziomonov4@gmail.com>
2025-09-20 14:48:37 +0500

    tlv protocol done

--- protocol/tlv.go
+++ protocol/tlv.go
@@ -1,1 +1,36 @@
 package tlv
+
+import "encoding/binary"
+
+const (
+	JOIN  = 1
+	MOVE  = 2
+	STATE = 3
+	WIN   = 4
+)
+
+const (
+	EMPTY = 0
+	X     = 1
+	O     = 2
+)
+
+func Encode(msgType byte, data []byte) []byte {
+	buf := make([]byte, 3+len(data))
+	buf[0] = msgType
+	binary.BigEndian.PutUint16(buf[1:3], uint16(len(data)))
+	copy(buf[3:], data)
+	return buf
+}
+
+func Decode(data []byte) (byte, []byte) {
+	if len(data) > 3 {
+		return 0, nil
+	}
+	msgType := data[0]
+	length := binary.BigEndian.Uint16(data[1:3])
+	if len(data) < int(3+length) {
+		return 0, nil
+	}
+	return msgType, data[3 : 3+length]
+}
