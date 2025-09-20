cf14fe07b3d79335e11b4a16299aeb710a662641
abdulaziz-go <abdulaziomonov4@gmail.com>
2025-09-20 14:20:38 +0500

    base strcuture set up

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"game/client"
+	"game/server"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("example running: go run main.go server|client")
+	}
+
+	switch os.Args[1] {
+	case "server":
+		server.Start()
+	case "client":
+		client.Start()
+	}
+}
